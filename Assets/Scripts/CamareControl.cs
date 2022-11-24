85a8e53ad958ba5c27bf3485f1b3295a5d1e5581
王川 <78136359+LosheMyWaifu@users.noreply.github.com>
2022-11-24 22:32:18 +0800

    动画调整

---
+++ Assets/Scripts/CamareControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CamareControl : MonoBehaviour
+{
+    public Camera pCamera;
+    public Transform pCameraFollowPoint;
+
+    //相机Z轴
+    [Header("相机Z轴")]
+    public float cameralayer = -10f;
+
+    //死区
+    [Header("相机跟随时间")]
+    public float cameraSpeed = 0.5f;
+
+    Vector3 currentVelocity;
+    Vector3 tempVec;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.pCamera = Camera.main;
+        pCameraFollowPoint = GameObject.Find("followPoint").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tempVec = new Vector3(pCameraFollowPoint.position.x, pCameraFollowPoint.position.y, cameralayer);
+    }
+
+    private void LateUpdate()
+    {
+        this.transform.position = Vector3.SmoothDamp(this.transform.position, tempVec, ref currentVelocity,cameraSpeed);
+    }
+}
--- Assets/Scripts/CharacterControl.cs
+++ Assets/Scripts/CharacterControl.cs
@@ -1,66 +1,142 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
+//角色状态
+public enum CHARACTER_STATE
+{
+    IDLE,
+    RUN,
+    JUMP,
+    FALL,
+    ATTACK,
+}
 public class CharacterControl : MonoBehaviour
 {
     public Transform mTrans;
     public Rigidbody2D mRigid2D;
     public Animator anim2D;
+    public new CapsuleCollider2D collider2D;
 
-    //�������
+    public CHARACTER_STATE characterState;
+
+    //玩家输入
     public string pMove = "Horizontal";
-    public string pJump = "Space";
-    public string pAttack = "X";
+    public string pJump = "Jump";
+    public string pAttack = "x";
 
 
-    //����
-    float moving;
+    //处理
+    float movement;
+    float jumping;
     bool ismoving;
+    bool isjumpingInput = false;//按下跳跃键？
 
-    //��ɫ����
+
+    public bool isGround;
+
+    //角色属性
     public float moveSpeed = 1;
+    public float jumpForce = 1;
+    public float fallthrehold = 1;
 
+    Collider2D[] results = new Collider2D[2];
+    ContactFilter2D contactF;
     // Start is called before the first frame update
     void Start()
     {
         mTrans = this.GetComponent<Transform>();
-        mRigid2D= this.GetComponent<Rigidbody2D>(); 
+        mRigid2D = this.GetComponent<Rigidbody2D>();
         anim2D = this.GetComponentInChildren<Animator>();
+        collider2D = this.GetComponent<CapsuleCollider2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        MoveCalculation();
-        
+        CheckGround();
+        RunMethed();
+        JumpMethed();
+        SwitchAnimation();
+        AttackMethed();
     }
 
     private void FixedUpdate()
     {
-        MoveImplement();
+         Vector2 tempVec = new Vector2(0, mRigid2D.velocity.y);
+         mRigid2D.velocity = new Vector2(movement, 0).normalized * moveSpeed*Time.deltaTime + tempVec;//移动        
     }
-
-    void MoveCalculation()
-    {  
-        moving = Input.GetAxis(pMove);
-        if(moving!=0)
-        {
-            ismoving = true;
-            mTrans.right = Vector2.right.normalized * moving;
-            anim2D.SetBool("isRunning", true);
-        }else
-        {
-           ismoving = false;
-            anim2D.SetBool("isRunning", false);
-        }        
-    }
-
-    void MoveImplement()
+    void RunMethed()
     {
-        if(ismoving)
+        anim2D.SetBool("isMoving", false);
+        movement = Input.GetAxis(pMove); //获取键盘输入
+        if (movement != 0)
         {
-            mRigid2D.velocity = new Vector2(moving, 0).normalized * moveSpeed;
+            mTrans.right = (Vector2.right * movement).normalized; //根据输入值转向
+            if(isGround)
+            {
+                anim2D.SetBool("isMoving", true);
+            }
+            
         }
     }
+
+    void JumpMethed()
+    {
+        //anim2D.SetBool("isJumping", false);
+        //anim2D.SetBool("isFalling", false);
+        isjumpingInput = Input.GetButtonDown(pJump);
+        if (isjumpingInput && isGround)
+        {
+            mRigid2D.AddForce(Vector2.up.normalized * jumpForce);
+            anim2D.SetBool("isJumping", true);
+        }
+    }
+
+    void SwitchAnimation()
+    {
+        anim2D.SetBool("isIdle", false);
+        if (anim2D.GetBool("isJumping"))
+        {
+            if(mRigid2D.velocity.y < -fallthrehold)
+            {
+                anim2D.SetBool("isJumping", false);
+                anim2D.SetBool("isFalling", true);
+            }
+        }else if(isGround)
+        {
+            anim2D.SetBool("isFalling", false);
+            if(!anim2D.GetBool("isMoving"))
+            {
+                anim2D.SetBool("isIdle", true);
+            }
+
+        }
+    }
+
+    void AttackMethed ()
+    {
+        //anim2D.SetBool("isAttacking", false);
+        if (Input.GetKeyDown(pAttack))
+        {
+            //todo
+            anim2D.SetTrigger("isAttacking");
+        }
+
+    }
+
+    void CheckGround()
+    {
+        if(collider2D.IsTouchingLayers(LayerMask.GetMask("Ground") ))
+        {
+            isGround= true; 
+        }else
+        {
+            isGround= false;
+        }
+        //Debug.Log(isGround);
+    }  
+   
 }
